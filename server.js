a355de1938a38e5ea0f831297c0a91bf14d19d5d
ringosung <ringosung@gmail.com>
2023-03-08 03:48:16 +0800

    set up mongoose connection

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts')
@@ -12,7 +16,10 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL),
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+const db = mongoose.connection;
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose.'))
 
 app.use('/', indexRouter)
 
