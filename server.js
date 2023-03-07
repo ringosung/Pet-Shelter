e163119e7b574aba69c3e5d7901cff346a6f457a
ringosung <ringosung@gmail.com>
2023-03-08 03:31:17 +0800

    set up MVC and routes

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL),
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000)
