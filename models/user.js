7c44f1da7be0dc355daa2e1386361294405b0122
ringosung <ringosung@gmail.com>
2023-03-15 03:41:46 +0800

    update input fields

---
+++ models/user.js
@@ -0,0 +1,13 @@
+// const mongoose = require('mongoose')
+//
+// const userSchema = new mongoose.Schema({
+//   email: String,
+//   password: String,
+//   googleId: String,
+//   facebookId: String,
+//   //userPost: postSchema
+// })
+//
+//
+//
+// module.exports = mongoose.model('User', userSchema)
--- server.js
+++ server.js
@@ -8,9 +8,29 @@ const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+
+// Authen
+// const { ROLE, users } = require('./data')
+// const { authUser, authRole } = require('./basicAuth')
+// const projectRouter = require('./routes/projects')
+
+
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
 const bookRouter = require('./routes/books')
+const initializePassport = require('./passport-config')
+
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
+
+const users = []
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -20,6 +40,19 @@ app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
+//Authen
+// app.use(express.json())
+// app.use(setUser)
+// app.use('/projects', projectRouter)
+// function setUser(req, res, next) {
+//   const userId = req.body.userId
+//   if (userId) {
+//     req.user = users.find(user => user.id === userId)
+//   }
+//   next()
+// }
+
+// Connect mongoDB
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
@@ -29,5 +62,79 @@ db.once('open', () => console.log('Connected to Mongoose'))
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
 app.use('/books', bookRouter)
+
+//Authen
+
+// app.get('/dashboard', authUser, (req, res) => {
+//   res.send('Dashboard Page')
+// })
+//
+// app.get('/admin', authUser, authRole(ROLE.ADMIN), (req, res) => {
+//   res.send('Admin Page')
+// })
+
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('index.ejs', { name: req.user.name })
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+  res.render('login.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render('register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
+  }
+})
+
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
+
 
 app.listen(process.env.PORT || 3000)
